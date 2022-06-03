4bf9020402a3bd730664f69319e89baa5e058307
gissehel <public-github@gissehel.org>
2022-06-03 07:28:49 +0000

    FEATURE : Add version to the page

--- src/view/App/App.js
+++ src/view/App/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import './index.css';
 import Stack from '../Stack';
+import GitInfo from 'react-git-info/macro'
+
+const gitInfo = new GitInfo();
+const version = `${process.env.REACT_APP_VERSION}-${gitInfo.commit.hash.substr(0, 8)}`;
 
 function App() {
+    
     return (
         <div className="App">
             <Stack />
-            <div class='AppInfo'>Sources: <a href='https://github.com/webgiss/replo'>https://github.com/webgiss/replo</a></div>
+            <div class='AppInfo'><span className='Name'>Replo</span> version <span className='Version'>{version}</span> - Sources: <a href='https://github.com/webgiss/replo'>https://github.com/webgiss/replo</a></div>
         </div>
     );
 }
