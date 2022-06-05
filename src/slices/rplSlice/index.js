5d8cc8b17ff97167501ad9bfb184f85f725230a4
gissehel <public-github@gissehel.org>
2022-06-05 11:48:35 +0000

    FEATURE : Support saving root in localStorage

--- src/slices/rplSlice/index.js
+++ src/slices/rplSlice/index.js
@@ -10,6 +10,7 @@ const rplSlice = createModule({
         error: null,
         keepInput: false,
         root: {},
+        rootLoaded: false,
     },
 
     reducers: {
@@ -19,8 +20,56 @@ const rplSlice = createModule({
         updateInput: (state, { input }) => {
             state.input = input
         },
+        rootLoad: (state, { keys }) => {
+            for (const key of keys) {
+                state.root[key.name] = key.value
+            }
+            state.rootLoaded = true
+        },
     },
 });
+
+const localStoragePrefix = 'dirroot-'
+
+rplSlice.stateHooks = [
+    {
+        name: 'store dir values in localStorage',
+        tester: (state) => state.root,
+        action: (state) => {
+            const keyGen = (name) => `${localStoragePrefix}${name}`
+
+            Object.keys(state.root).forEach((name) => {
+                const key = keyGen(name)
+                const value = JSON.stringify(state.root[name])
+                if (localStorage.getItem(key) !== value) {
+                    localStorage.setItem(key, value)
+                }
+            })
+        },
+    },
+    {
+        name: 'Load dir values from localStorage',
+        tester: (state) => state.rootLoaded,
+        action: (state, dispatch) => {
+            const keyTest = (key) => key.startsWith(localStoragePrefix)
+            const keyExtract = (key) => key.slice(localStoragePrefix.length, key.length)
+            const keys = []
+            if (state.rootLoaded) {
+                for (const key of Object.keys(localStorage)) {
+                    if (keyTest(key)) {
+                        const name = keyExtract(key)
+                        const value = JSON.parse(localStorage.getItem(key))
+                        keys.push({ name, value })
+                    }
+                }
+            }
+
+            dispatch(rplSlice.actions.rootLoad({ keys }))
+        },
+    },
+
+
+];
 
 export const actions = rplSlice.actions;
 export default rplSlice;
